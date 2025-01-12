296e8fd6c802b20f2201e00206e56de18d52e216
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2025-01-12 16:05:37 +0530

    add balance

--- backend/routes/account.routes.js
+++ backend/routes/account.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getbalance, transferFunds } from '../controllers/account.controller.js';
+import { addBalance, getbalance, transferFunds } from '../controllers/account.controller.js';
 import { isSignedIn } from '../middlewares/user.middlewares.js';
 
 const router=express.Router();
 router.post("/transaction",isSignedIn,transferFunds);
 router.get("/getBalance",isSignedIn,getbalance);
+router.get("/addBalance",isSignedIn,addBalance);
 export default router;
